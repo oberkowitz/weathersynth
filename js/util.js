ccd0b1974f7dc3ec75f825331a6ba612a886136e
Oren Berkowitz <oren.berkowitz@soundcloud.com>
2017-04-12 20:06:19 +0200

    Beginning to experiment with one data file

---
+++ js/util.js
@@ -0,0 +1,22 @@
+var ardmap = function(x, inMin, inMax, outMin, outMax) { // Map value x in range [inMin, inMax] to a value in range [outMin, outMax]
+	// Ex: ardmap(5, 0, 10, 2, 4) == 3
+	return (x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+function pad(n, width, z) {
+	z = z || '0';
+	n = n + '';
+	return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
+
+function isLeapYear(year) {  
+	return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0);  
+}  
+
+function randomDate(start, end) {
+	return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+}
+
+Array.prototype.extend = function (other_array) {
+	/* you should include a test to check whether other_array really is an array */
+	other_array.forEach(function(v) {this.push(v)}, this);    
+}
--- yearlyscraper.sh
+++ yearlyscraper.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-currentdate=$2-01-01
+startdate=$2-01-01
 loopenddate=$2-12-31
+currentdate=$startdate
 
 airport=$1
 
@@ -17,11 +18,26 @@ until [ "$currentdate" == "$loopenddate" ]
 do
 	day=$(gdate --date "$currentdate" +%d)
 	month=$(gdate --date "$currentdate" +%m)
-	year=$(gdate --date "$currentdate" +%y)
+	year=$(gdate --date "$currentdate" +%Y)
 
+	filename="weatherData/$airport/$month-$day-$year.tsv"
 	echo "Starting $currentdate"
-	./tablescrape.py https://www.wunderground.com/history/airport/$airport/2016/$month/$day/DailyHistory.html 5 > "weatherData/$airport/$month-$day-$year.tsv"
-	echo "Finished $currentdate"
+	./tablescrape.py https://www.wunderground.com/history/airport/$airport/$year/$month/$day/DailyHistory.html 5 > "$filename"
+	
+	# prefix the date to the time
+	sed -i -e "2,\$s/^/$currentdate /" "$filename"
 
+	# Output all the data to one big file
+	outputFile="./weatherData/$airport/${airport}-${year}-complete.tsv"
+	if [ "$currentdate" == "$startdate" ]; then
+		cat "$filename" >> "$outputFile"
+	else
+		tail -n +2 "$filename" >> "$outputFile"
+	fi
+
+	echo "Finished $currentdate"
 	currentdate=$(gdate --date "$currentdate day 1" +%Y-%m-%d)
 done
+
+
+
