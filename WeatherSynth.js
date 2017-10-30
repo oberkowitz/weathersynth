d97d2a98aafb6d43f43e39e7430136bf8f5abfbb
oberkowitz <ojberkowitz@gmail.com>
2017-10-30 12:03:30 +0100

    Keyboard A triggers notes, and buffer sources are stopped at the end of the release or when the note is retriggered

--- WeatherSynth.js
+++ WeatherSynth.js
@@ -1,7 +1,10 @@
-function WeatherSynth(dataBuffer) {
+function WeatherSynth(dataBuffer, audioCtx, canvas) {
 	this.audioCtx = audioCtx;
+	this.canvas = canvas;
 	this.dataBuffer = dataBuffer;
 	this.currentBuffer = dataBuffer;
+	this.analyser = this.audioCtx.createAnalyser();
+	this.analyser.connect(this.audioCtx.destination);
 	this.activeKeys = {};
 }
 
@@ -24,16 +27,17 @@ WeatherSynth.prototype.createBufferForNote = function(note, velocity) {
 	}
 
 	source.buffer = buffer;
-	source.loop = true;
+	source.loop = true; // TODO: Looping the source prevents automatic garbage collection, release it when finished
 	source.playbackRate.value = playbackRate;
 
 	source.connect(gain);
 	gain.connect(biquadFilter);
 	envelope.connect(gain.gain);
-	biquadFilter.connect(this.audioCtx.destination);
+	biquadFilter.connect(this.analyser);
 	biquadFilter.type = "lowpass";
 	biquadFilter.frequency.value = 10000;
 
+	this.startVisualizer(this.analyser);
 	return {
 		"source": source,
 		"envelope": envelope
@@ -52,14 +56,77 @@ function frequencyFromNote(note) {
 }
 
 WeatherSynth.prototype.noteOn = function(note, velocity) {
-	if (this.activeKeys[note]) delete this.activeKeys[note];
+	if (this.activeKeys[note]) {
+		// this.activeKeys[note].source.stop();
+		delete this.activeKeys[note];
+	}
 	this.activeKeys[note] = this.createBufferForNote(note, velocity);
 	this.activeKeys[note].source.start();
 	this.activeKeys[note].envelope.trigger();
+	console.log(this.activeKeys);
 
 }
 
 WeatherSynth.prototype.noteOff = function(note) {
-	if (!this.activeKeys[note]) return;
-	this.activeKeys[note].envelope.release();
+	var note = this.activeKeys[note];
+	if (!note) return;
+	note.envelope.release();
+	var that = this;
+	setTimeout(function() {
+		console.log(note);
+		note.source.stop();
+		console.log("stop");
+	}, Math.floor(note.envelope.releaseTime * 1000));
+}
+
+WeatherSynth.prototype.startVisualizer = function(analyser) {
+	WIDTH = this.canvas.width;
+	HEIGHT = this.canvas.height;
+	var canvasCtx = this.canvas.getContext("2d");
+
+	analyser.fftSize = 2048;
+	var bufferLength = analyser.fftSize;
+	console.log(bufferLength);
+	var dataArray = new Uint8Array(bufferLength);
+
+	canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+
+	var that = this;
+	function draw() {
+
+		drawVisual = requestAnimationFrame(draw);
+
+		analyser.getByteTimeDomainData(dataArray);
+
+		canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+		canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+		canvasCtx.lineWidth = 2;
+		canvasCtx.strokeStyle = 'rgb(255, 255, 255)';
+
+		canvasCtx.beginPath();
+
+		var sliceWidth = WIDTH * 1.0 / bufferLength;
+		var x = 0;
+
+		for(var i = 0; i < bufferLength; i++) {
+
+			var v = dataArray[i] / 128.0;
+			var y = v * HEIGHT/2;
+
+			if(i === 0) {
+			  canvasCtx.moveTo(x, y);
+			} else {
+			  canvasCtx.lineTo(x, y);
+			}
+
+			x += sliceWidth;
+		}
+
+		canvasCtx.lineTo(that.canvas.width, that.canvas.height/2);
+		canvasCtx.stroke();
+	}
+
+	draw();
 }
