b19f71e52c982053043dba9aab8a1e2a6548b487
Oren Berkowitz <ojberkowitz@gmail.com>
2024-11-20 14:57:14 +0100

    Adding option for windowing for zero crossings on wavetable endings

--- wav/json_interp.py
+++ wav/json_interp.py
@@ -2,6 +2,7 @@
 import numpy as np
 import datetime
 from scipy.io.wavfile import write
+from scipy.signal.windows import tukey
 import json
 
 # To avoid confusion, the following definition of words will be used:
@@ -17,34 +18,41 @@ def load_from_json_file(fpath):
     return d
 
 # Assumes x is monotonically increasing
-def generate_wavetable(x, y, num_samples: int):
+def generate_wavetable(x, y, num_samples: int, windowing=False):
     ## use linspace for even spacing between start and end
     x2 = np.linspace(x.min(), x.max(), num_samples)
     interpolated = np.interp(x2, x, y)
-    return interpolated
+    normalized = 2.*(interpolated - np.min(interpolated))/np.ptp(interpolated)-1
+    
+    if windowing:
+        window = tukey(num_samples, alpha = .1, sym=True) ## increasing alpha applies windowing to more of the signal. Default = .5, max = 1.0
+        output = np.multiply(normalized, window)
+    else: 
+        output = normalized
+
+    return output
 
 # Assumes x-axis will be `datetimeEpoch`
 def generate_yearlong_wavetable(year_json, y_name: str, num_samples: int):
     days = np.array(year_json['days'])
     xys = map(lambda h: (h['datetimeEpoch'], h[y_name]), days)
     npxys = np.array(list(xys), dtype = [('datetimeEpoch', 'i4'), (y_name, 'f4')])
-    return generate_wavetable(npxys["datetimeEpoch"], npxys[y_name], num_samples)
+    return generate_wavetable(npxys["datetimeEpoch"], npxys[y_name], num_samples, True)
 
+response_1970 = load_from_json_file("/Users/oberk/git/weathersynth/wav/data/1970/Berlin_1970.json")
 response_1976 = load_from_json_file("/Users/oberk/git/weathersynth/wav/data/1976/Berlin_1976.json")
 response_1992 = load_from_json_file("/Users/oberk/git/weathersynth/wav/data/1992/Berlin_1992.json")
 response_2008 = load_from_json_file("/Users/oberk/git/weathersynth/wav/data/2008/Berlin_2008.json")
 response_2024 = load_from_json_file("/Users/oberk/git/weathersynth/wav/data/2024/Berlin_2024.json")
 
+wavetable_1970 = generate_yearlong_wavetable(response_1970, "tempmax", 1024)
 wavetable_1976 = generate_yearlong_wavetable(response_1976, "tempmax", 1024)
 wavetable_1992 = generate_yearlong_wavetable(response_1992, "tempmax", 1024)
 wavetable_2008 = generate_yearlong_wavetable(response_2008, "tempmax", 1024)
 wavetable_2024 = generate_yearlong_wavetable(response_2024, "tempmax", 1024)
 
-final_audio = np.concatenate((wavetable_1976, wavetable_1992, wavetable_2008, wavetable_2024))
-#Normalized
-final_audio = 2.*(final_audio - np.min(final_audio))/np.ptp(final_audio)-1
-
-file_name = f"test-{datetime.datetime.now().microsecond}.wav"
+final_audio = np.concatenate((wavetable_1970, wavetable_1976, wavetable_1992, wavetable_2008, wavetable_2024))
+file_name = f"test-{datetime.datetime.now().ctime()}.wav"
 write(file_name, int(SAMPLE_RATE), final_audio.astype(np.float32))
 
 print(f"Wrote {len(final_audio)} samples to {file_name}")
